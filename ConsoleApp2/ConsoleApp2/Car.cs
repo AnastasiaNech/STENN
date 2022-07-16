8036a439e957c53c16b84d71599d810701ffdb58
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2022-07-16 17:34:51 +0300

    Приложение 2

---
+++ ConsoleApp2/ConsoleApp2/Car.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp2
+{
+    public class Car
+    {
+        public Car(string model, string color, string description)
+        {
+            Model = model;
+            Color = color;
+            Description = description;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Description { get; set; }
+
+        public bool Equals(Car car)
+        {
+            if (Model == car.Model &&
+                Color == car.Color)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Model, Color);
+        }
+
+        public static List<Car> CreateCars()
+        {
+            return new List<Car>()
+        {
+            new Car("Cadillac Srx", "Red", "New"),
+            new Car("Cadillac Srx", "Red", "Old"),
+            new Car("Cadillac Srx", "Yellow", "Total"),
+            new Car("Cadillac Srx", "Yellow", "Total"),
+            new Car("Ford F-150", "Yellow", "Old"),
+            new Car("Ford F-150", "Black", "Total"),
+            new Car("Cadillac Sts", "Yellow", "Old"),
+            new Car("Cadillac Sts", "Yellow", "Old"),
+            new Car("Ford Mustang", "White", "Old"),
+            new Car("Ford Mustang", "Black", "Total"),
+            new Car("Cadillac Escalade", "White", "Total"),
+            new Car("Ford Focus", "Black", "New"),
+            new Car("Ford F-150", "Black", "Old"),
+            new Car("Ford Mustang", "Black", "Total")
+        };
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp2;
+
+var cars = Car.CreateCars();
+var firsCar = cars[0];
+
+Console.WriteLine("First car: " + firsCar.GetHashCode());
+
+cars.ForEach(x =>
+{
+    Console.WriteLine(firsCar.Equals(x));
+    Console.WriteLine(x.GetHashCode());
+});
