da7153a59544136d09d8f8495d1aa93cd713091b
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2022-07-16 16:52:10 +0300

    Приложение 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿int X = 10;
+int Y = 20;
+int Z = 30;
+
+ChangeValue(ref X,ref Y,ref Z);  
+
+Console.WriteLine(X.ToString() + " " + Y.ToString() + " " + Z.ToString());
+
+
+static void ChangeValue(ref int X, ref int Y, ref int Z)
+   {
+    X = X ^ Y;
+    Y = X ^ Y;
+    X = X ^ Y;
+
+    Y = Y ^ Z;
+    Z = Z ^ Y;
+    Y = Y ^ Z;
+}
