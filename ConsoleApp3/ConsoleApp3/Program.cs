0b1f92d3d4390d15d122f0c64b16d22ef501d3e9
AnastasiaNech <nechaevaanastasiamih@gmail.com>
2022-07-16 17:49:04 +0300

    Приложение 3

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Tree
+{
+    public Tree(string name, IEnumerable<Tree> children)
+    {
+        Name = name;
+        Children = children;
+    }
+
+    public string Name { get; set; }
+
+    public IEnumerable<Tree> Children { get; set; }
+
+    public IEnumerable<string>  GetChildNames()
+    {
+        return Children.Select(x => x.Name);
+    }
+
+}
